2fafeac06caf6747a9ac2a7ace6fd19cc963013e
Angel <angel.santiago@tutanota.com>
2023-07-22 18:37:12 -0400

    Server base setup

--- server/index.ts
+++ server/index.ts
@@ -1,106 +1,77 @@
-const winston = require("winston");
+const Hapi = require('@hapi/hapi');
+const NeDB = require('nedb');
 
-const logger = winston.createLogger({
-  level: "debug",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+// Initialize NeDB
+const db = new NeDB({ filename: 'papers.db', autoload: true });
+
+// Initialize Hapi server
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
 });
 
-module.exports = logger;
+// Route to fetch all papers
+server.route({
+    method: 'GET',
+    path: '/papers',
+    handler: (request, h) => {
+        return new Promise((resolve, reject) => {
+            db.find({}, (err, docs) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(docs);
+                }
+            });
+        });
+    }
+});
 
-logger.log("debug", "Hello, Winston!");
-logger.debug("The is the home '/' route.");
-logger.error("Events Error: Unauthenticated user");
+// Route to fetch papers for a specific day
+server.route({
+    method: 'GET',
+    path: '/papers/{date}',
+    handler: (request, h) => {
+        return new Promise((resolve, reject) => {
+            db.find({ date: request.params.date }, (err, docs) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(docs);
+                }
+            });
+        });
+    }
+});
 
-// const openai = require('openai');
-// const fs = require('fs');
-// const pdf = require('pdf-parse');
-// const fetch = require('node-fetch');
-// const yaml = require('yaml');
-// const util = require('util');
-// const ora = require('ora');
-// const textwrap = require('textwrap');
+// Route to fetch paper by id
+server.route({
+    method: 'GET',
+    path: '/paper/{id}',
+    handler: (request, h) => {
+        return new Promise((resolve, reject) => {
+            db.findOne({ id: request.params.id }, (err, doc) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(doc);
+                }
+            });
+        });
+    }
+});
 
-// const prompts = [
-//   'Can you give me a very clear explanation of the core assertions, implications, and mechanics elucidated in this paper?',
-//   "Can you explain the value of this in basic terms? Like you're talking to a CEO. So what? What's the bottom line here?",
-//   'Can you give me an analogy or metaphor that will help explain this to a broad audience.',
-// ];
+// Start the server
+const start = async () => {
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-// // File operations
-// const saveFile = (filepath, content) => {
-//   fs.writeFileSync(filepath, content, 'utf8');
-// };
+    console.log('Server running at:', server.info.uri);
+};
 
-// const openFile = (filepath) => {
-//   return fs.readFileSync(filepath, 'utf8');
-// };
-
-// const saveYaml = (filepath, data) => {
-//   fs.writeFileSync(filepath, yaml.stringify(data), 'utf8');
-// };
-
-// const openYaml = (filepath) => {
-//   return yaml.parse(fs.readFileSync(filepath, 'utf8'));
-// };
-
-// // API functions
-// const chatbot = async (conversation, model = "gpt-4-0613", temperature = 0) => {
-//   const spinner = ora('Thinking...').start();
-//   try {
-//     const response = await openai.ChatCompletion.create({
-//       model: model,
-//       messages: conversation,
-//       temperature: temperature,
-//     });
-//     const text = response['choices'][0]['message']['content'];
-//     spinner.stop();
-//     return { text, total_tokens: response['usage']['total_tokens'] };
-//   } catch (error) {
-//     console.error(`Error communicating with OpenAI: "${error}"`);
-//     spinner.stop();
-//     throw error;
-//   }
-// };
-
-// const chatPrint = (text) => {
-//   const formattedText = textwrap.wrap(text, { width: 120 });
-//   console.log(`\n\n\nCHATBOT:\n\n${formattedText}`);
-// };
-
-// // Main function
-// const main = async () => {
-//   openai.apiKey = openFile('key_openai.txt').trim();
-
-//   const pdfFiles = fs.readdirSync('input/').filter(f => f.endsWith('.pdf'));
-
-//   for (const pdfFile of pdfFiles) {
-//     const filename = 'output/' + pdfFile.replace('.pdf', '.txt');
-//     if (fs.existsSync(filename)) {
-//       continue;
-//     }
-
-//     const dataBuffer = fs.readFileSync('input/' + pdfFile);
-//     const paper = await pdf(dataBuffer);
-//     let paperText = paper.text;
-//     if (paperText.length > 22000) {
-//       paperText = paperText.substring(0, 22000);
-//     }
-
-//     let allMessages = [{ role: 'system', content: paperText }];
-//     let report = '';
-//     for (const p of prompts) {
-//       allMessages.push({ role: 'user', content: p });
-//       const { text } = await chatbot(allMessages);
-//       chatPrint(text);
-//       allMessages.push({ role: 'assistant', content: text });
-//       report += `\n\n\n\nQ: ${p}\n\nA: ${text}`;
-//     }
-
-//     saveFile(filename, report.trim());
-// };
-
-// main().catch(console.error);
-
-
-'[ total of'
+start();
