64dd91c24601541bebc98a5f8b870e66a83c1e7c
Angel <angel.santiago@tutanota.com>
2023-08-13 03:23:13 -0400

    Similarity filter using reference papers

--- server/src/test.ts
+++ server/src/test.ts
@@ -1,34 +1,47 @@
-import scrapePapersByDate from "./config/scrape-papers-by-date";
+import scrapeRefPapers from "./config/scrape-ref-papers";
 import { promises as fs } from 'fs';
 import path from 'path';
 
+const entryIds =  [
+  '2307.09909',
+  '2307.06159',
+  '2307.05082',
+  '2307.05300',
+  '2305.10601',
+  // '2307.05071', // Mining for Unknown Unknowns
+  '2307.01933',
+  '2307.01577',
+  '2307.01548',
+  '2307.01403',
+  '2307.01204',
+  '2307.02276',
+  '2307.02046',
+  '2307.02295',
+  // '2307.02243', // Power-up! What Can Generative Models Do for Human Computation Workflows? (Crowdsourcing AI)
+  // '2307.01951', // A Neural Collapse Perspective on Feature Evolution in Graph Neural Networks
+  '2307.01928',
+  '2307.02477',
+  '2307.02485',
+  '1801.07243v5',
+  // ---------------- claude
+  '2307.02390',
+  '2307.07255',
+  // '2307.06950', // Pathway toward prior knowledge-integrated machine learning in engineering
+  '2307.06917',
+  '2307.08962', // !
+  '2206.08853',
+  '2307.09364', // ? Local Minima Drive Communications in Cooperative Interaction
+  '2307.08859', // ? Curriculum Learning for Graph Neural Networks: A Multiview Competence-based Approach
+  '2307.09721',
+  '2210.02441',
+  '2307.10680',
+]
 
-function getDatesForMonth(month: number, year: number) {
-  return Array.from({ length: new Date(year, month, 0).getDate() }, (_, i) => {
-    const date = new Date(Date.UTC(year, month - 1, i + 1)).toUTCString();
-    return date.split(' ').slice(0, 4).join(' ');
-  });
-}
-
-// const dates = getDatesForMonth(8, 2023); // For August 2023
-// console.log(dates);
-
-function getTodaysDate() {
-  const date = new Date().toUTCString();
-  return date.split(' ').slice(0, 4).join(' ');
-}
-
-console.log(getTodaysDate());
-
-const testDate = 'Fri, 11 Aug 2023'
-
-// scrapePapersByDate(getTodaysDate());
-
-scrapePapersByDate(testDate).then((arxivPapers) => {
+scrapeRefPapers(entryIds).then((arxivPapers) => {
 
   // console.log('arxivPapers: ', arxivPapers);
   const root = '/Users/spankyed/Develop/Projects/PdfToVid/server/src/files';
-  const jsonPath = path.join(root, 'output', 'data', 'arxiv-papers.json');
+  const jsonPath = path.join(root, 'input', 'ref-papers.json');
   const jsonString = JSON.stringify(arxivPapers, null, 2);
 
   // fs.writeFile('arxiv-papers.json', jsonString)
@@ -39,10 +52,7 @@ scrapePapersByDate(testDate).then((arxivPapers) => {
     .catch((err) => {
       console.error('Error writing to file:', err);
     });
-
 });
-
-
 
 
 
