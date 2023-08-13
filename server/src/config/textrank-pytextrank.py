bd2e24d83207dc44534bab1eb890dd9f1a9a9a33
Angel <angel.santiago@tutanota.com>
2023-08-13 00:37:05 -0400

    User defined boosted keywords for text rank

--- server/src/config/textrank-pytextrank.py
+++ server/src/config/textrank-pytextrank.py
@@ -14,12 +14,34 @@ nlp = spacy.load("en_core_web_sm")
 # Add pytextrank to the spaCy pipeline
 nlp.add_pipe("textrank")
 
+# User-defined keywords and their boost factor
+user_keywords = {
+    "education": .5,
+    "learning": .5,
+    "knowledge graph": 1.5,
+    "generative agents": 1.8,
+    # "3d": 1.5,
+    # "causal": 1.5,
+    # "graph": 1.5
+}
+
 output_data = []
 
 for paper in data:
     combined_text = paper['title'] + ". " + paper['abstract']  # Concatenate title and abstract
     doc = nlp(combined_text)
-    key_terms = [(phrase.text, phrase.rank) for phrase in doc._.phrases[:10]]  # Extracting top 5 keywords with their relevancy scores
+    
+    # Extract keywords and their scores
+    key_terms = [(phrase.text, phrase.rank) for phrase in doc._.phrases]
+    
+    # Boost scores of user-defined keywords
+    for i, (term, score) in enumerate(key_terms):
+        if term in user_keywords:
+            key_terms[i] = (term, score * user_keywords[term])
+    
+    # Re-rank keywords based on updated scores
+    key_terms = sorted(key_terms, key=lambda x: x[1], reverse=True)[:15]
+    
     paper_output = {
         "id": paper["id"],
         "title": paper["title"],
@@ -30,8 +52,8 @@ for paper in data:
     output_data.append(paper_output)
 
 # Write the output data to a new JSON file
-output_path = '/Users/spankyed/Develop/Projects/PdfToVid/server/src/files/output/data/keywords_scores_output_pytextrank_combined.json'
+output_path = '/Users/spankyed/Develop/Projects/PdfToVid/server/src/files/output/data/keywords_scores_boosted_output_pytextrank_combined.json'
 with open(output_path, 'w') as output_file:
     json.dump(output_data, output_file, indent=4)
 
-print(f"Keywords with scores written to {output_path}")
+print(f"Boosted keywords with scores written to {output_path}")
