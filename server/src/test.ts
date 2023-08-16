a0199b40754675f9b1888d97a66964cc76ca2497
Angel <angel.santiago@tutanota.com>
2023-08-16 07:35:03 -0400

    Rename config folder to functions

--- server/src/test.ts
+++ server/src/test.ts
@@ -1,4 +1,4 @@
-import scrapeRefPapers from "./config/scrape-ref-papers";
+import scrapeRefPapers from "./functions/scrape-ref-papers";
 import { promises as fs } from 'fs';
 import path from 'path';
 
