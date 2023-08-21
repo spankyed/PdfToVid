2cfa1ca9da4f96ecfa6eb6ed81d1b3fd65e0e687
Angel <angel.santiago@tutanota.com>
2023-08-21 05:15:48 -0400

    Code stash

--- server/src/http/utils.ts
+++ server/src/http/utils.ts
@@ -252,3 +252,27 @@ async function wipeAllDatastores(): Promise<void> {
     await datastore.removeAsync({}, { multi: true });
   };
 };
+
+
+function createQueue(){
+  const queue: any[] = [];
+  let isProcessing = false;
+
+  return {
+    push: (item: any) => {
+      queue.push(item);
+      if (!isProcessing) {
+        processQueue();
+      }
+    },
+  };
+
+  async function processQueue() {
+    isProcessing = true;
+    while (queue.length) {
+      const item = queue.shift();
+      await item();
+    }
+    isProcessing = false;
+  }
+};
