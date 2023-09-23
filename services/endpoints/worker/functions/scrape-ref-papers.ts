e0e43612be0782bf7f734f948d5790d74b26c1d9
Angel <angel.santiago@tutanota.com>
2023-09-23 03:21:01 -0400

    Simplify scrape-ref-papers

--- services/endpoints/worker/functions/scrape-ref-papers.ts
+++ services/endpoints/worker/functions/scrape-ref-papers.ts
@@ -1,50 +1,34 @@
-import { chromium, Page, BrowserContext } from 'playwright';
+import axios from 'axios';
+import { parseStringPromise } from 'xml2js';
 
 interface Entry {
   id: string;
   title: string;
   abstract: string;
-  author: { name: string }[];
   pdfLink: string;
-  published: string;
 }
 
-const extractPaperDetails = async (context: BrowserContext, link: string): Promise<Entry> => {
-  const page = await context.newPage();
-  await page.goto(link);
-  
-  const id = await page.$eval('[name="citation_arxiv_id"]', (node: HTMLMetaElement) => node.content);
-  const title = await page.$eval('.title', (node: HTMLElement) => 
-    node.textContent?.replace("Title:", "").trim() || ''); // remove "Title:" from title
-  const abstract = await page.$eval('.abstract', (node: HTMLElement) => 
-    node.textContent?.replace("Abstract:", "").trim() || ''); // remove "Abstract:" from abstract
-  const author = await page.$$eval('.authors a', (nodes: HTMLElement[]) => nodes.map(n => ({ name: n.textContent?.trim() || '' })));
+const ARXIV_OAI_ENDPOINT = 'http://export.arxiv.org/oai2?';
 
-  // const pdfLink = await page.$eval('.full-text a', (node: HTMLAnchorElement) => node.href);
-  // https://arxiv.org/pdf/2308.05713.pdf
-  const pdfLink = `https://arxiv.org/pdf/${id}.pdf`
-  
-  const published = await page.$eval('[name="citation_date"]', (node: HTMLMetaElement) => node.content);
+const fetchPaperDetails = async (id: string): Promise<Entry> => {
+    const query = `verb=GetRecord&identifier=oai:arXiv.org:${id}&metadataPrefix=arXiv`;
+    const response = await axios.get(ARXIV_OAI_ENDPOINT + query);
 
-  await page.close();
+    if (response.status !== 200) {
+        throw new Error('Error fetching data from ArXiv OAI endpoint');
+    }
 
-  // return { id, title, abstract, author, pdfLink, published };
-  return { id, title, abstract, pdfLink };
+    const parsedData = await parseStringPromise(response.data);
+    const metadata = parsedData['OAI-PMH'].GetRecord[0].record[0].metadata[0]['arXiv'][0];
+
+    return {
+        id: metadata.id[0],
+        title: metadata.title[0],
+        abstract: metadata.abstract[0],
+        pdfLink: `http://arxiv.org/pdf/${metadata.id[0]}.pdf`
+    };
 };
 
-export default async function scrapeRefPapers(ids: string[]) {
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  let paperDetails: Entry[] = [];
-
-  const page = await context.newPage();
-
-  const links = ids.map(id => `https://arxiv.org/abs/${id}`);
-
-  const paperDetailsPromises = links.map(link => extractPaperDetails(context, link));
-  paperDetails = await Promise.all(paperDetailsPromises);
-
-  await browser.close();
-
-  return paperDetails
-};
+export default async function fetchPapers(ids: string[]): Promise<Entry[]> {
+    return await Promise.all(ids.map(fetchPaperDetails));
+}
