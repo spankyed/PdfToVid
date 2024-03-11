a6e5e765961da03cd1f2db8778767bf33f470683
Angel <angel.santiago@tutanota.com>
2024-03-11 01:56:39 -0400

    Relocate papers list component for loading ui refactor

--- client/src/dashboard/papers/index.tsx
+++ client/src/dashboard/papers/index.tsx
@@ -1,92 +1,45 @@
 import React, { useCallback, useContext, useState } from 'react';
-import { Box, Typography } from '@mui/material';
-import {  useNavigate } from 'react-router-dom';
-import EmptyState from '~/shared/components/Empty';
-import Scraping from '~/shared/components/Scraping';
-import Ranking from '~/shared/components/Ranking';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import { useAtom } from 'jotai';
-import { papersListAtom, selectedDayAtom } from '../../shared/state';
-import { formatDate } from '~/shared/utils/dateFormatter';
+import { papersListAtom } from '../../shared/state';
 import PapersList from './list';
-import CustomTooltip from '~/dashboard/papers/popover/summary';
 
 function Papers(): React.ReactElement {
-  const navigate = useNavigate();
   // const store = useContext<StoreType>(StoreContext);
   // const { state, papersList, selectedDay, selectDay} = store.dashboard;
 
   const [papersList] = useAtom(papersListAtom);
-  const [selectedDay, setSelectedDay] = useAtom(selectedDayAtom);
-
-  const reformatDateMemo = useCallback((inputDate: string): string => {
-    return formatDate(inputDate, {
-      weekday: 'short',
-      month: 'short',
-      day: '2-digit',
-    });
-  }, []);
-  
-  const onDayClick = day => e => {
-    const is = tag => e.target.tagName === tag;
-    const ignore = is('BUTTON') || is('path') || is('svg') || is('LI');
-
-    if (ignore) return;
-
-    navigate(`/day/${day.value}`);
-  }
 
   // const is = a => b => a === b;
-
+  const noData = papersList.length === 0;
+  
   return (
     <>
-      {papersList.map(({ day, papers }) => {
-        const { value, status } = day;
-
-        const contentByStatus = {
-          pending: <Empty day={value} />,
-          scraping: <Scraping />,
-          ranking: <Ranking />,
-          complete: <PapersList papers={papers} />,
-        };
-        
-        return (
-          <Box 
-            key={'day-' + value} 
-            // onMouseEnter={() => setSelectedDay(value)}
-            sx={{ 
-              display: 'flex', 
-              flexDirection: 'column', 
-              alignItems: 'center', 
-              borderBottom: '1px solid rgba(0, 0, 0, 0.3)',
-              paddingTop: 2,  
-              paddingBottom: 2,
-              backgroundColor: selectedDay === value ? 'rgba(25, 118, 210, 0.08)' : 'transparent',
-              cursor: 'pointer',
-            }}
-            onClick={onDayClick(day)}
-          >
-            <Typography variant="h5" style={{ textDecoration: 'none', marginBottom: 4, marginTop: '.5em' }} >
-              {reformatDateMemo(value)}
-            </Typography>
-            {
-              contentByStatus[status]
-            }
-          </Box>
-        );
-      })}
-
-      <CustomTooltip/>
+      { noData 
+        ? <NoData />
+        : <PapersList papersList={papersList} />
+      }
     </>
   );
 }
 
-
-function Empty({ day }: { day: string }): React.ReactElement {
-  return (
-    <Box display="flex" flexDirection="column" alignItems="center" gap={3} margin={3}>
-      <EmptyState day={day}/>
-    </Box>
-  );
-}
+const NoData = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 4,
+      textAlign: 'center',
+      color: 'grey.500'
+    }}
+  >
+    <CircularProgress />
+    <Typography variant="subtitle1" sx={{ marginTop: 2 }}>
+      Loading data...
+    </Typography>
+  </Box>
+);
 
 export default Papers;
--- client/src/dashboard/papers/list.tsx
+++ client/src/dashboard/papers/list.tsx
@@ -1,11 +1,93 @@
-import React, { useState } from 'react';
-import { Pagination } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+import { Box, Pagination, Typography } from '@mui/material';
 import Thumbnail from '~/shared/components/Thumbnail';
-import { Paper } from '~/shared/utils/types';
+import { Paper, PapersList } from '~/shared/utils/types';
 import { useAtom } from 'jotai';
 import { anchorElAtom, isOpenAtom, popoverTargetAtom, tooltipRefAtom } from './popover/store';
+import CustomTooltip from '~/dashboard/papers/popover/summary';
+import EmptyState from '~/shared/components/Empty';
+import Scraping from '~/shared/components/Scraping';
+import Ranking from '~/shared/components/Ranking';
+import { selectedDayAtom } from '~/shared/state';
+import { useNavigate } from 'react-router-dom';
+import { formatDate } from '~/shared/utils/dateFormatter';
 
-function PapersList({ papers }: { papers: Paper[] }): React.ReactElement {
+function PapersListV2({ papersList }: { papersList: PapersList[] }): React.ReactElement {
+  const [selectedDay, setSelectedDay] = useAtom(selectedDayAtom);
+  const navigate = useNavigate();
+
+  const reformatDateMemo = useCallback((inputDate: string): string => {
+    return formatDate(inputDate, {
+      weekday: 'short',
+      month: 'short',
+      day: '2-digit',
+    });
+  }, []);
+  
+  const onDayClick = day => e => {
+    const is = tag => e.target.tagName === tag;
+    const ignore = is('BUTTON') || is('path') || is('svg') || is('LI');
+
+    if (ignore) return;
+
+    navigate(`/day/${day.value}`);
+  }
+
+  return (
+    <>
+    {papersList.map(({ day, papers }) => {
+      const { value, status } = day;
+
+
+      const contentByStatus = {
+        pending: <Empty day={value} />,
+        scraping: <Scraping />,
+        ranking: <Ranking />,
+        complete: <List papers={papers} />,
+        // noData: <NoData />,
+      };
+      
+      return (
+        <Box 
+          key={'day-' + value} 
+          // onMouseEnter={() => setSelectedDay(value)}
+          sx={{ 
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'center', 
+            borderBottom: '1px solid rgba(0, 0, 0, 0.3)',
+            paddingTop: 2,  
+            paddingBottom: 2,
+            backgroundColor: selectedDay === value ? 'rgba(25, 118, 210, 0.08)' : 'transparent',
+            cursor: 'pointer',
+          }}
+          onClick={onDayClick(day)}
+        >
+          <Typography variant="h5" style={{ textDecoration: 'none', marginBottom: 4, marginTop: '.5em' }} >
+            {reformatDateMemo(value)}
+          </Typography>
+          {
+            contentByStatus[status]
+          }
+        </Box>
+      );
+    })}
+
+    <CustomTooltip/>
+  </>
+  )
+}
+
+function Empty({ day }: { day: string }): React.ReactElement {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" gap={3} margin={3}>
+      <EmptyState day={day}/>
+    </Box>
+  );
+}
+
+
+function List({ papers }: { papers: Paper[] }): React.ReactElement {
   const [currentPage, setCurrentPage] = useState(1);
   const [previousPage, setPreviousPage] = useState(2);
   const [, setAnchorEl] = useAtom(anchorElAtom);
@@ -83,4 +165,4 @@ function PapersList({ papers }: { papers: Paper[] }): React.ReactElement {
   ); 
 }
 
-export default PapersList;
+export default PapersListV2;
