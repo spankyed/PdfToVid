3a0e44693a313ab8b9d9b07db7ec628e4a06a807
Angel <angel.santiago@tutanota.com>
2024-03-13 01:56:02 -0400

    Add count to date entry title

--- client/src/date-entry/index.tsx
+++ client/src/date-entry/index.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 // import { Link } from 'react-router-dom';
-import { Typography, Box, Tabs, Tab, Button, Grid, TextField, CircularProgress, LinearProgress } from '@mui/material';
+import { Typography, Box, Tabs, Tab, Button, Grid, TextField, CircularProgress, LinearProgress, Badge, styled } from '@mui/material';
 // import { Paper, StoreType } from '~/shared/store';
 // import { StoreContext } from '..';
 import PapersTable from './PapersTable';
@@ -14,7 +14,22 @@ import { fetchPapersForDayAtom, dayPageStateAtom } from '~/shared/state';
 // import SearchIcon from '@mui/icons-material/Search';
 import { useParams } from 'react-router-dom'; // Import useParams
 import { formatDateParts } from '~/shared/utils/dateFormatter';
+import { getColorShade } from '~/shared/utils/getColorShade';
 
+const ScoreBadge = styled(Badge)<{ count: number }>(({ theme, count }) => ({
+  '& .MuiBadge-badge': {
+    top: 8,
+    right: '100%',
+    transform: 'translateX(50%)',
+    backgroundColor: getColorShade(count / 90),
+    color: theme.palette.common.white,
+    borderRadius: theme.shape.borderRadius,
+    padding: '4px 8px',
+    fontWeight: 'bold',
+    letterSpacing: '0.2em',
+    border: '1px solid rgba(255, 255, 255, 0.4)',
+  },
+}));
 
 function Day(): React.ReactElement {
   let { dayId } = useParams<{ dayId: string }>();
@@ -39,13 +54,13 @@ function Day(): React.ReactElement {
 
   return (
     <Box padding={3} sx={{ marginTop: 3, margin: '0 auto', maxWidth: '90%' }}>
-      <PageTitle date={dayId} />
+      <PageTitle date={dayId} count={papers.length} />
       {componentsByState[state]}
     </Box>
   );
 }
 
-const PageTitle: React.FC<{ date: string }> = ({ date }) => {
+const PageTitle: React.FC<{ date: string, count: number }> = ({ date, count }) => {
   // const [formattedDate, weekday] = useMemo(() => {
     const formattedDate = useMemo(() => {
       const [weekday, month, day, year] = formatDateParts(date, {
@@ -60,21 +75,29 @@ const PageTitle: React.FC<{ date: string }> = ({ date }) => {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center" marginBottom={1}>
-      <Typography variant="h4"
-        sx={{
-          background: '#FE6B8B',
-          color: 'white',
-          webkitBackgroundClip: 'text',
-          webkitTextFillColor: 'transparent',
-          borderBottom: '2px solid #FF8E53',
-          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-          padding: '.5em 2em .5em 2em',
-          fontWeight: 'bold',
-          borderRadius: '5px',
-          letterSpacing: '0.0075em',
-          marginTop: '20px',
-        }}
-      >{formattedDate}</Typography>
+      <ScoreBadge 
+        badgeContent={`${count}`} 
+        count={count}
+      >
+
+        <Typography variant="h4"
+          sx={{
+            background: '#FE6B8B',
+            color: 'white',
+            webkitBackgroundClip: 'text',
+            webkitTextFillColor: 'transparent',
+            borderBottom: '2px solid #FF8E53',
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+            padding: '.5em 2em .5em 2em',
+            fontWeight: 'bold',
+            borderRadius: '5px',
+            letterSpacing: '0.0075em',
+            marginTop: '20px',
+          }}
+        >
+          {formattedDate}
+        </Typography>
+      </ScoreBadge>
     </Box>
   );
 }
