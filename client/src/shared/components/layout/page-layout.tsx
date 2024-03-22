60ff1763d045361d65b27805a8efc5a76879a5e3
Angel <angel.santiago@tutanota.com>
2024-03-22 02:57:57 -0400

    Scroll to top functionality

--- client/src/shared/components/layout/page-layout.tsx
+++ client/src/shared/components/layout/page-layout.tsx
@@ -1,17 +1,56 @@
+import React, { useState, useEffect, ReactNode, createRef } from 'react';
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { forwardRef } from 'react';
 
-const height = 'calc(100vh - 65px)'
+interface PageLayoutProps {
+  children: ReactNode;
+  compact?: boolean;
+}
 
-const PageLayout = ({ children, compact = true, ...props }) => {
+const height = 'calc(100vh - 65px)';
+
+const PageLayout = forwardRef<HTMLDivElement, PageLayoutProps>(({
+  children,
+  compact = true,
+  ...props
+}, ref: React.ForwardedRef<HTMLDivElement>) => {
+  const [showScroll, setShowScroll] = useState(false);
+  ref = ref || createRef<HTMLDivElement>();
+
+  const checkScrollTop = () => {
+    if (ref && 'current' in ref && ref.current) {
+      const { scrollTop } = ref.current;
+      setShowScroll(scrollTop > 200);
+    }
+  };
+
+  useEffect(() => {
+    const element = ref && 'current' in ref ? ref.current : null;
+
+    if (element) {
+      element.addEventListener('scroll', checkScrollTop);
+      return () => element.removeEventListener('scroll', checkScrollTop);
+    }
+  }, [ref]);
+
+  const scrollTop = () => {
+    if (ref && 'current' in ref && ref.current) {
+      ref.current.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
   return (
     <Box
+      ref={ref} // Use the forwarded ref
       sx={{
         overflowY: 'auto',
         flexGrow: 1,
         height,
         justifyContent: 'center',
         display: 'flex',
-    }}
+      }}
       {...props}
     >
       <div
@@ -23,9 +62,23 @@ const PageLayout = ({ children, compact = true, ...props }) => {
         }}
       >
         {children}
+        {showScroll && (
+          <Fab
+            color="secondary"
+            size="small"
+            onClick={scrollTop}
+            sx={{
+              position: 'fixed',
+              bottom: '20px',
+              right: '20px',
+            }}
+          >
+            <KeyboardArrowUpIcon />
+          </Fab>
+        )}
       </div>
     </Box>
   );
-};
+});
 
 export default PageLayout;
