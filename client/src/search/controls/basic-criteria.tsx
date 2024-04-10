81fa126b87fb0b9e423247a3f1a71b0c9bc3acd8
Angel <angel.santiago@tutanota.com>
2024-04-10 00:04:33 -0400

    Add % adornment to relevancy score input

--- client/src/search/controls/basic-criteria.tsx
+++ client/src/search/controls/basic-criteria.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useAtom } from 'jotai';
-import { Select, MenuItem, FormControl, TextField, Checkbox, FormControlLabel, FormGroup, Grid } from '@mui/material';
+import { Select, MenuItem, FormControl, TextField, Checkbox, FormControlLabel, FormGroup, Grid, InputAdornment } from '@mui/material';
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { favoriteAtom, viewedAtom, relevancyAtom, comparisonOperatorAtom } from '../store';
@@ -49,8 +49,8 @@ const BasicCriteriaControl: React.FC<{}> = () => {
             sx={{ marginRight: 2 }}
             onChange={(e) => setComparisonOperator(e.target.value)}
           >
-            <MenuItem value="0">≥</MenuItem>
-            <MenuItem value="1">≤</MenuItem>
+            <MenuItem value="≥">≥</MenuItem>
+            <MenuItem value="≤">≤</MenuItem>
           </Select>
 
           <TextField
@@ -58,7 +58,10 @@ const BasicCriteriaControl: React.FC<{}> = () => {
             label="Relevancy Score"
             variant="outlined"
             type="number"
-            InputProps={{ inputProps: { min: 0, max: 100 } }}
+            InputProps={{
+              inputProps: { min: 0, max: 100 },
+              endAdornment: <InputAdornment position="end">%</InputAdornment>
+            }}
             value={relevancy}
             onChange={(e) => setRelevancy(e.target.value)}
             sx={{ minWidth: 170 }} 
