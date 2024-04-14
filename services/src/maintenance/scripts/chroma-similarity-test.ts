2f194ea4d0d579c9b10de064985ad857146210e7
Angel <angel.santiago@tutanota.com>
2024-04-14 03:39:44 -0400

    Code cleanup

--- services/src/maintenance/scripts/chroma-similarity-test.ts
+++ services/src/maintenance/scripts/chroma-similarity-test.ts
@@ -57,7 +57,7 @@ async function createSBertEmbeddingFunction(modelName: string) {
   return { generate };
 }
 
-async function testSimilarity(doc1) {
+async function testSimilarity(doc1: any) {
   const MODEL_NAME = "Xenova/all-MiniLM-L6-v2";
   const embedder = await createSBertEmbeddingFunction(MODEL_NAME);
   let [paper_embedding]: number[][] = await embedder.generate([doc1]); // Assuming encode returns an array of numbers.
@@ -80,7 +80,7 @@ async function testSimilarity(doc1) {
   console.log('relevancy_score: ', relevancy_score);
 
 }
-async function testSimilarity2(doc1) {
+async function testSimilarity2(doc1: any) {
   const MODEL_NAME = "Xenova/all-MiniLM-L6-v2";
   const embedder = await createSBertEmbeddingFunction(MODEL_NAME);
   let [paper_embedding]: number[][] = await embedder.generate([doc1]); // Assuming encode returns an array of numbers.
