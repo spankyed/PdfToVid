013fa362b5a149399a27b25f422f318ff820d147
Angel <angel.santiago@tutanota.com>
2024-04-14 04:09:41 -0400

    Update chroma, reuse embedder, store new references in collection

--- services/src/maintenance/scripts/create-chroma-ref-collection.ts
+++ services/src/maintenance/scripts/create-chroma-ref-collection.ts
@@ -1,20 +1,23 @@
-import chromadb from "chromadb";
+// import chromadb from "chromadb";
+import { ChromaClient } from 'chromadb'
 import * as fs from "fs";
 import { createEmbedder } from "../../shared/embedder";
 
-const client = new chromadb.ChromaClient();
+const client = new ChromaClient();
 const COLLECTION_NAME = "paper-embeddings";
 const PATH_REF_PAPERS =
   "/Users/spankyed/Develop/Projects/CurateGPT/services/database/generated/research-papers.json";
 
-let embedder: { generate: (texts: string[]) => Promise<number[][]> };
 
 async function storePaperEmbeddingsInChroma(
   papers: any[],
   collectionName = COLLECTION_NAME
 ) {
   const existingCollections = await client.listCollections();
+  const embedder = await createEmbedder();
+
   console.log('existingCollections: ', existingCollections);
+
   if (existingCollections.map((c) => c.name).includes(collectionName)) {
     await client.deleteCollection({ name: COLLECTION_NAME });
     await client.createCollection({ name: collectionName,
@@ -22,37 +25,36 @@ async function storePaperEmbeddingsInChroma(
       metadata: { "hnsw:space": "cosine" }
     });
   }
-  const embeddings = await embedder.generate(
-    papers.map((paper) => paper.title + ". " + paper.abstract)
-  );
+  // const embeddings = await embedder.generate(
+  //   papers.map((paper) => paper.title + ". " + paper.abstract)
+  // );
   const collection = await client.getCollection({
     name: collectionName,
     embeddingFunction: embedder,
   });
 
   await collection.add({
-    embeddings: embeddings,
+    // embeddings: embeddings,
     documents: papers.map((paper) => paper.title + ". " + paper.abstract),
     ids: papers.map((paper) => paper.id),
   });
 }
 
 async function fetchAndPrintCollectionContent() {
-  embedder = await createEmbedder();
-  const collection = await client.getCollection({ name: COLLECTION_NAME, embeddingFunction: embedder });
+  const collection = await client.getCollection({ name: COLLECTION_NAME });
 
-  // Assuming there's a method to fetch all documents (or a subset) from the collection
   // const documents = await collection.get();
-
   // console.log(JSON.stringify(documents, null, 2));
 
   const refPapers = JSON.parse(fs.readFileSync(PATH_REF_PAPERS, "utf-8"));
   // console.log('refPapers: ', refPapers);
   await storePaperEmbeddingsInChroma(refPapers);
 
-  const documents2 = await collection.get();
+  console.log('done');
 
-  console.log(JSON.stringify(documents2, null, 2));
+  // const documents2 = await collection.get();
+
+  // console.log(JSON.stringify(documents2, null, 2));
 }
 
 fetchAndPrintCollectionContent();
