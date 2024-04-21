a7aff6d65917747fcb0a3ea9d20120fa749ac9fa
Angel <angel.santiago@tutanota.com>
2024-04-20 21:49:49 -0400

    Code cleanup

--- services/src/shared/utils/get-config.ts
+++ services/src/shared/utils/get-config.ts
@@ -1,5 +1,9 @@
 import config from '@config'; // Ensure this import works as expected, and adjust the import path if necessary
 
 export function getConfig() {
+  if (!config && !(config as any).default){
+    return {}
+  }
+
   return (config as any).default
 }
