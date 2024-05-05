b52206195c4ca3b59245ef25c17b870d4fe6e538
Angel <angel.santiago@tutanota.com>
2024-05-04 20:50:23 -0400

    Remove virtualized table attempts

--- client/src/date-entry/components/table.tsx
+++ client/src/date-entry/components/table.tsx
@@ -6,104 +6,69 @@ import Favorite from '~/shared/components/paper/favorite';
 import Relevancy from '~/shared/components/paper/relevancy';
 import { paperStates } from '~/shared/utils/paperStates';
 import PaperAction, { RejectAction } from '~/shared/components/paper/paper-action';
-
-// import SearchIcon from '@mui/icons-material/Search';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 
-const PapersTable: React.FC<{ papers?: Paper[]; isLoading?: boolean; placeholderRows?: number }> = ({
-  papers = [],
-  isLoading = false,
-  placeholderRows = 5
-}) => {
-  return (
-    <TableContainer sx={{ marginTop: 3, margin: '0 auto', minWidth: '100%' }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Paper Title</TableCell>
-            <TableCell align="left"></TableCell>
-            <TableCell align="center">Status</TableCell>
-            <TableCell align="right" style={{ paddingRight: '3em' }}>Actions</TableCell>
-          </TableRow>
-        </TableHead>
+const Row = ({paper, index}) => {
+  const statusType = paperStates[paper.status]
+  const notUploaded = paper.status !== PaperState.published
+  const showReject = paper.status === PaperState.approved
 
-        <TableBody>
-          {
-            isLoading
-            ? <TablePlaceholder placeholderRows={placeholderRows}/>
-            : <TableRows papers={papers}/>
+  return (
+    <TableRow key={index}>
+      <TableCell 
+        align="left" 
+        sx={{ 
+          fontSize: '1.075rem', padding: 0,
+          '&:hover': {
+            backgroundColor: 'rgba(25, 118, 210, 0.08)',
           }
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
-
-const TableRows = ({papers}) => {
-  const statusType = (paper: Paper) => paperStates[paper.status]
-  const notUploaded = (paper: Paper) => paper.status !== PaperState.published
-  const showReject = (paper: Paper) => paper.status === PaperState.approved
-
-  return (
-    <>
-    {papers.map((paper, index) => (
-      <TableRow key={index}>
-        <TableCell 
-          align="left" 
-          sx={{ 
-            fontSize: '1.075rem', padding: 0,
-            '&:hover': {
-              backgroundColor: 'rgba(25, 118, 210, 0.08)',
-            }
+        }}
+      >
+        <Link 
+          to={`/paper/${paper.id}`} 
+          style={{ 
+            display: 'block', 
+            padding: '1em', 
+            textDecoration: 'none', 
+            color: 'inherit', 
           }}
         >
-          <Link 
-            to={`/paper/${paper.id}`} 
-            style={{ 
-              display: 'block', 
-              padding: '1em', 
-              textDecoration: 'none', 
-              color: 'inherit', 
-            }}
-          >
-            <Relevancy paper={paper} />
-            {paper.title}
-          </Link>
-        </TableCell>
-        <TableCell align="center">
-          <Favorite paper={paper}/>
-        </TableCell>
-        <TableCell align="center">
-          <Chip 
-            label={statusType(paper).label} 
-            color={statusType(paper).color} 
-            title={statusType(paper).title}
-          />
-        </TableCell>
-        <TableCell align="right">
-          <ButtonGroup variant="text" aria-label="paper actions">
-            {
-              // notUploaded(paper) && (
-                <PaperAction paper={paper} />
-              // )
-            }
-            <Button>
-              <Tooltip title='View'>
-                <Link to={`/paper/${paper.id}`}>
-                  <VisibilityIcon color="info" style={{ marginRight: '4px' }} />
-                </Link>
-              </Tooltip>
-            </Button>
-            {
-              showReject(paper) && (
-                <RejectAction paper={paper}/>
-              )
-            }
-          </ButtonGroup>
-        </TableCell>
-      </TableRow>
-    ))}
-    </>
+          <Relevancy paper={paper} />
+          {paper.title}
+        </Link>
+      </TableCell>
+      <TableCell align="center">
+        <Favorite paper={paper}/>
+      </TableCell>
+      <TableCell align="center">
+        <Chip 
+          label={statusType.label} 
+          color={statusType.color} 
+          title={statusType.title}
+        />
+      </TableCell>
+      <TableCell align="right">
+        <ButtonGroup variant="text" aria-label="paper actions">
+          {
+            // notUploaded && (
+              <PaperAction paper={paper} />
+            // )
+          }
+          <Button>
+            <Tooltip title='View'>
+              <Link to={`/paper/${paper.id}`}>
+                <VisibilityIcon color="info" style={{ marginRight: '4px' }} />
+              </Link>
+            </Tooltip>
+          </Button>
+          {
+            showReject && (
+              <RejectAction paper={paper}/>
+            )
+          }
+        </ButtonGroup>
+      </TableCell>
+    </TableRow>
   )
 }
 
@@ -129,5 +94,36 @@ const TablePlaceholder = ({ placeholderRows }) => {
     </>
   );
 };
+
+const PapersTable: React.FC<{ papers?: Paper[]; isLoading?: boolean; placeholderRows?: number }> = ({
+  papers = [],
+  isLoading = false,
+  placeholderRows = 5
+}) => {
+  return (
+    <TableContainer sx={{ marginTop: 3, margin: '0 auto', minWidth: '100%' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Paper Title</TableCell>
+            <TableCell align="left"></TableCell>
+            <TableCell align="center">Status</TableCell>
+            <TableCell align="right" style={{ paddingRight: '3em' }}>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {
+            isLoading
+            ? <TablePlaceholder placeholderRows={placeholderRows}/>
+            : papers.map((paper, index) => (
+              <Row key={index} paper={paper} index={index} />
+            ))
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 export default PapersTable;
