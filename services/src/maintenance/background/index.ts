c28c7eb8c7e95ea3e224f1e7fbbfe8546d8773f9
Angel <angel.santiago@tutanota.com>
2024-05-09 23:10:50 -0400

    Code cleanup

--- services/src/maintenance/background/index.ts
+++ services/src/maintenance/background/index.ts
@@ -1,6 +1,6 @@
 import { getConfig } from "~/shared/utils/get-config";
 import { ensureReferenceCollectionExists } from "../scripts/ensure-reference-collection";
-import { scrapeBatch } from "../scripts/scrape-batch";
+// import { scrapeBatch } from "../scripts/scrape-batch";
 import { startJobAddNewDates, startJobScrapeNewDatesWithRetry } from "../scripts/cron-jobs";
 import { backFillAbsentDates } from "../scripts/add-dates";
 
