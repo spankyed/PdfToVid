512ec9e05e85e49df44b0e752d46df8aadddb3c9
Angel <angel.santiago@tutanota.com>
2024-05-23 04:15:46 -0400

    Hide document section from chat ui

--- client/src/paper-entry/chat/index.tsx
+++ client/src/paper-entry/chat/index.tsx
@@ -24,7 +24,7 @@ export default function ChatTab({ paperId }) {
   return (
     <Box sx={{ marginTop: 2, mb: 1 }}>
       <ChatOptions />
-      <DocumentSection />
+      {/* <DocumentSection /> */}
       <MessageList />
     </Box>
   );
