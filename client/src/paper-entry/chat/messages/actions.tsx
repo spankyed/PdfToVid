9fbd9887ac33dffc3cc19c7a815e5c74e781f21a
Angel <angel.santiago@tutanota.com>
2024-05-24 02:23:24 -0400

    Show different actions depending on message state

--- client/src/paper-entry/chat/messages/actions.tsx
+++ client/src/paper-entry/chat/messages/actions.tsx
@@ -10,6 +10,7 @@ import { messagesAtom } from './store';
 import * as api from '~/shared/api/fetch';
 import { addNewThreadAtom, branchThreadAtom, selectedThreadsAtom } from '../threads/store';
 import { paperAtom } from '~/paper-entry/store';
+import { StopCircleOutlined } from '@mui/icons-material';
 
 const capitalize = (s) => (s && s[0].toUpperCase() + s.slice(1)) || '';
 
@@ -19,6 +20,7 @@ const actions = [
   { name: 'show', icon: <VisibilityIcon />, color:'#43a047' },
   { name: 'hide', icon: <VisibilityOffIcon />, color:'#fdd835' },
   { name: 'thread', icon: <AltRouteIcon />, color:'#1e88e5' },
+  { name: 'stop', icon: <StopCircleOutlined />, color:'yellow' },
 ];
 
 export default function Actions({ message }) {
@@ -27,20 +29,26 @@ export default function Actions({ message }) {
   const branchThread = useSetAtom(branchThreadAtom);
 
   const filters = {
-    regenerate: (m) => m.role === 'assistant',
-    show: (m) => m.hidden,
-    hide: (m) => !m.hidden,
-    // thread: (m) => !m.hidden,
+    regenerate: (m) => !m.streaming && m.role === 'assistant',
+    show: (m) => !m.streaming && m.hidden,
+    hide: (m) => !m.streaming && !m.hidden,
+    thread: (m) => !m.streaming,
+    // streaming actions below
+    stop: (m) => m.streaming && m.role === 'assistant',
   }
 
   const filteredActions = actions.filter(action => filters[action.name] ? filters[action.name](message) : true);
 
   const handlers = {
-    // regenerate: async () => {
-    //   setMessages(messages.filter(m => m.id !== message.id));
-    //   const res = await api.regenerateMessage(message.id)
-    // // todo stream new message
-    // },
+    regenerate: async () => {
+      // setMessages(messages.filter(m => m.id !== message.id));
+      // const res = await api.regenerateMessage(message.id)
+      // todo stream new message
+    },
+    stop: async () => {
+      // setMessages(messages.filter(m => m.id !== message.id));
+      // await api.stopMessage(message.id)
+    },
     delete: async () => {
       setMessages(messages.filter(m => m.id !== message.id));
       await api.deleteMessage(message.id)
