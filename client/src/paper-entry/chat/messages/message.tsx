c60240a5f713a0227ddd66386bdc8e01bc543730
Angel <angel.santiago@tutanota.com>
2024-06-05 01:15:10 -0400

    Markdown viewer for messages

--- client/src/paper-entry/chat/messages/message.tsx
+++ client/src/paper-entry/chat/messages/message.tsx
@@ -6,66 +6,73 @@ import Actions from './actions';
 import { paperAtom } from '~/paper-entry/store';
 import { selectedThreadsAtom } from '../threads/store';
 import { colors } from '~/shared/styles/theme';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import './message.css';
 
 export default function Message({ message }) {
   const paper = useAtomValue(paperAtom);
   const selectedThreads = useAtomValue(selectedThreadsAtom);
   const currThread = selectedThreads[paper!.id];
-  const isBranchMessage = message.parentId && currThread?.messageId == message.parentId;
+  const isBranchMessage = message.parentId && currThread?.messageId === message.parentId;
 
   const [actionsShowing, showActions] = useState(false);
   const isAssistant = message.role === 'assistant';
   const isHidden = message.hidden;
   const yellowRGBA = 'rgba(255, 235, 59, 0.1)';
   const blueRGBA = colors.palette.background.paper;
-  // const blueRGBA = 'rgba(33, 150, 243, 0.3)';
   const opacity = isHidden ? 0.4 : 1;
 
   return (
-    // <Box mb={2} p={2} sx={{ textAlign: 'left' }} className={`${isAssistant ? 'bg-slate-100' : ''}`}>
     <Box
       mb={2}
       p={2}
       sx={{
         textAlign: 'left',
         whiteSpace: 'pre-wrap',
-        // backgroundColor: isAssistant ? 'rgba(0, 0, 0, 0.1)' : '',
         backgroundColor:
           isBranchMessage ? blueRGBA :
           isHidden ? yellowRGBA :
           isAssistant ? 'rgba(0, 0, 0, 0.1)' : '',
       }}
-      className=''
       onMouseEnter={() => showActions(true)}
       onMouseLeave={() => showActions(false)}
-      >
-
-      {/* <Typography variant="caption">{new Date(message.timestamp).toLocaleString()}</Typography> */}
+    >
       <div className='flex items-center'>
         <Tooltip
           title={dayjs(message.timestamp).format('MMM D, YYYY h:mm A')}
-          placement='top'>
-          <p style={{
-            fontWeight: '600',
-            color: isAssistant ? colors.palette.secondary.light : 'rgba(255, 255, 255, 0.65)',
-            opacity
-          }} className='pr-4  pb-1'>
-            { isAssistant
-            ? 'Assistant'
-            : 'You'
-            }
+          placement='top'
+        >
+          <p
+            style={{
+              fontWeight: '600',
+              color: isAssistant ? colors.palette.primary.light : colors.palette.secondary.light,
+              // color: isAssistant ? colors.palette.primary.light : 'rgba(255, 255, 255, 0.65)',
+              opacity,
+            }}
+            className='pr-4 pb-1'
+          >
+            {isAssistant ? 'Assistant' : 'You'}
           </p>
         </Tooltip>
-
-        {
-          actionsShowing && !message.stream &&
-          <Actions message={message}/>
-        }
+        {actionsShowing && !message.stream && <Actions message={message} />}
       </div>
-      <Typography sx={{ opacity }}>
-        {message.text}
-      </Typography>
+      <Box sx={{ opacity, display: 'flex', flexDirection: 'column' }}>
+        <ReactMarkdown
+          skipHtml={true}
+          remarkPlugins={[remarkGfm]}
+          components={{
+            h3: ({ node, ...props }) => <div><h3 className="custom-h3" {...props} /></div>,
+            p: ({ node, ...props }) => <div className="custom-p" {...props} />,
+            ul: ({ children }) => <ul className="custom-list-ul">{children}</ul>,
+            ol: ({ children }) => <ol className="custom-list-ol">{children}</ol>,
+            li: ({ children }) => <li className="custom-list-item">{children}</li>
+          }}
+        >
+          {message.text}
+        </ReactMarkdown>
+      </Box>
     </Box>
   );
-};
+}
 
